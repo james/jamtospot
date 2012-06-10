cc384224fe6c5d340ae8ece08cf6779d6c1404a4
Sean Lerner <seanslerner@gmail.com>
2012-06-09 17:56:30 -0700

    styled

--- jamtospot.rb
+++ jamtospot.rb
@@ -10,7 +10,32 @@ class JamToSpot < Sinatra::Base
   ######################################################
 
   get '/' do 
-    track = SpotSearch::Base.search('shake your rump')
     erb :index
+  end
+
+  post '/' do
+    redirect "/result/#{params[:username]}"
+  end
+
+  get '/result/:username' do
+    jammed = Jammed.new
+    jams = jammed.jams(params[:username])
+    @jam_count = jams.length
+    jam_titles = []
+    jam_artists = []
+    jams.each do |j|
+      jam_titles << j['title']
+      jam_artists << j['artist']
+    end
+
+    @data = []
+
+    jam_titles.each_with_index do |track, i|
+      artist = jam_artists[i]
+      @data << [artist, track, SpotSearch::Base.search(track, artist)]
+    end
+    p @data
+
+    erb :result
   end
 end
--- spot-search.rb
+++ spot-search.rb
@@ -8,21 +8,25 @@ module SpotSearch
     include HTTParty
     base_uri "http://ws.spotify.com"
 
-    def self.search(string)
+    def self.search(string, artist)
       query = {:q => string}
       response = get("/search/#{API_VERSION}/track.json", 
         :query => query, 
         :format => :plain)
       response_j = JSON.parse(response.body)
-      earliest = 2020
+      #earliest = 2020
       result = ''
       response_j['tracks'].each do |track|
-        if track['album']['released'].to_i < earliest
-          earliest = track['album']['released'].to_i
+        #released = track['album']['released'].to_i
+        #if released < earliest && track['artists'][0]['name'] == artist
+        if track['artists'][0]['name'] == artist
+          p track['artists'][0]['name']
+          #earliest = released
           result = track
+          break
         end
       end
-      p result['href']
+      return result['href']
     end
   end
 end
